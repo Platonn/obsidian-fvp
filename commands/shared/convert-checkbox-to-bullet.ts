afee5907f19b5367a6dcf5f55f4962b0ac204356
Krzysztof Platis <platonn.git@gmail.com>
2023-07-22 10:59:20 +0200

    fix(converCheckboxToBullet): dont convert filled checkbox but only empty one

--- commands/shared/convert-checkbox-to-bullet.ts
+++ commands/shared/convert-checkbox-to-bullet.ts
@@ -5,5 +5,5 @@
  * Note: it preserves the original indentation.
  */
 export function convertCheckboxToBullet(line: string): string {
-	return line.replace(/(^\s*)-\s\[.]\s/gm, "$1- ");
+	return line.replace(/(^\s*)-\s\[ ]\s/gm, "$1- ");
 }
